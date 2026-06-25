504b486ecd07ff39d70635bb7ca27d45907c6ce0
HarshSharma1080 <harshsharma752136@gmail.com>
2026-06-25 19:35:52 +0530

    added to the git

---
+++ git.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    age = 100;
+    cout << age << endl;
+    cout << "this file is created the track using git and learn more about the git" << endl;
+    cout << "hello git" << endl;
+
+
+
+}
