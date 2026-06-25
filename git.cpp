18d380eac02daafedf456049806b8369f9ea166c
HarshSharma1080 <harshsharma752136@gmail.com>
2026-06-25 19:35:52 +0530

    this is the v1 of the git.cpp

--- git.cpp
+++ git.cpp
@@ -4,8 +4,10 @@ int main(){
     int age;
     age = 100;
     cout << age << endl;
-    cout << "this file is created the track using git and learn more about the git" << endl;
-    cout << "hello git" << endl;
+    cout << "this is the v1 of the git cpp " << endl;
+    
+    cout << "the v1 is working quite good right now " << endl;
+    
 
 
 }
