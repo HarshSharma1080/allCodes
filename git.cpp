fd30fcd4b7751b235233b63c8afb21face90b309
HarshSharma1080 <harshsharma752136@gmail.com>
2026-06-25 19:35:52 +0530

    this is the v2 of the git.cpp

--- git.cpp
+++ git.cpp
@@ -4,10 +4,10 @@ int main(){
     int age;
     age = 100;
     cout << age << endl;
-    cout << "this is the v1 of the git cpp " << endl;
+    cout << "this is the v2 of the git cpp " << endl;
     
-    cout << "the v1 is working quite good right now " << endl;
-    
+    cout << "the v2 is not working as expected right now " << endl;
+
 
 
 }
