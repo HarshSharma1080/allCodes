fc1ab4fd172bacdf7c499613c84330d53009d6fb
HarshSharma1080 <harshsharma752136@gmail.com>
2026-06-28 10:48:53 +0530

    modified the dev-jsm

--- tp.cpp
+++ tp.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout << "this is to demonstrate the merge conflicts, edited by the dev-jsm" << endl;    
+    cout << "this is to demonstrate the merge conflicts, edited by the dev-jsm" << endl;
+    
 }
 
