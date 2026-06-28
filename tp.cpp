b7bec2913b4508c842fc3cc5b8d8f492e01abd0c
HarshSharma1080 <harshsharma752136@gmail.com>
2026-06-28 09:08:06 +0530

    added a cout telling bout the dev-jsm branch

--- tp.cpp
+++ tp.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout << " hi this is file created to demonstrate the merge conflicts" << endl;    
+    cout << " hi this is file created to demonstrate the merge conflicts" << endl;   
+    cout << "heyy yooo man, this is the dev-jsm branch of the main, " << endl;
 }
 
