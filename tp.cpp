0b864cfab02ce13a0cba3d817421a144aed5ba8d
HarshSharma1080 <harshsharma752136@gmail.com>
2026-06-28 09:43:29 +0530

    edited by the dev-jsm

--- tp.cpp
+++ tp.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout << " hi this is file created to demonstrate the merge conflicts" << endl;    
+    cout << "this is to demonstrate the merge conflicts, edited by the dev-jsm" << endl;    
 }
 
