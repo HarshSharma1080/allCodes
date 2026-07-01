00abf12a6abc058effa3d66e24c842e01835d542
HarshSharma1080 <harshsharma752136@gmail.com>
2026-07-01 11:42:53 +0530

    use the stash feature, to save the working directory for temperary to work on other thing urgently

--- tp.cpp
+++ tp.cpp
@@ -5,6 +5,11 @@ int main(){
     cout << "hello github !" << endl;
     cout << "hello from dev-harsh " << endl;
     
+    cout << "this is the half finished feature #1" << endl;
+    cout << "this is the half finished feature #2" << endl;
+    cout << "this is the half finished feature #3" << endl;
+
+    
     cout << "this is new imp feature" << endl;
     cout << "this is the day saving feature" << endl;
     
