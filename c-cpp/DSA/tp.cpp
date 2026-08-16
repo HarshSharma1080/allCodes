48d3082fc46a16019aeb2330f6c9c2902a274f61
HarshSharma1080 <harshsharma752136@gmail.com>
2026-08-16 16:05:56 +0530

    struct in heap

--- c-cpp/DSA/tp.cpp
+++ c-cpp/DSA/tp.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
 using namespace std;
-
+struct rectangle {
+    int length;
+    int breadth;
+};
 int main(){
-    cout << "hello harsh, how are you"<< endl;
+    struct rectangle r1;
+    r1.length = 8;
+    r1.breadth = 11;
+
+    struct rectangle *p = &r1;
+    cout << "the length is: " << p -> length << endl;
+    cout << "the breadth is: " << p -> breadth << endl;
+
+    struct rectangle *ptr;
+    ptr = (struct rectangle*)malloc(sizeof(struct rectangle));
+    ptr -> length = 111;
+    ptr -> breadth = 222;
+    cout << ptr -> length << endl;
+    cout << ptr -> breadth << endl;
+
 }
